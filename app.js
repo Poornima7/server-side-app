70d5abca5090ca0cc9ea9120ce95f08d1dea76e4
Muniraj <muniraj@outlined.co>
2018-10-13 22:08:36 +0530

    index routes

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+//index routes
+app.get('/', (req,res) => {
+ res.send('bharathi');
+});
+
+app.get('/about',(req,res)=>{
+    res.send('About');
+})
+
+
+
 app.listen(port, () =>{
     console.log(`server started on port ${port}`);
 });
