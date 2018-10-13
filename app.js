18cddd15702f5cc82122caa5bef21b7f1ec71608
Muniraj <muniraj@outlined.co>
2018-10-13 22:51:28 +0530

    handlebarjs middleware, views, layouts, mainhandlebar, abouthandlebar

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require('express');
+var exphbs  = require('express-handlebars');
 
 // app to initiallize
 const app = express();
 const port = 5000;
 
+//Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//middleware
+app.use(function (req, res, next) {
+    console.log('Time:', Date.now())
+    next()
+  })
+
+
 //index routes
-app.get('/', (req,res) => {
- res.send('bharathi');
+// app.get('/', (req,res) => {
+//  res.send('bharathi');
+// });
+
+app.get('/', function (req, res) {
+    res.render('home');
 });
 
-app.get('/about',(req,res)=>{
-    res.send('About');
-})
-
+app.get('/about', (req,res)=>{
+    res.render('about');
+});
 
 
 app.listen(port, () =>{
