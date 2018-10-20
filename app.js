3d2d029e8ccf6360151ca3d07ff61d9a055c6295
Muniraj <muniraj@outlined.co>
2018-10-20 18:47:24 +0530

    curd operation

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 var exphbs  = require('express-handlebars');
+var methodOverride = require('method-override')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session')
 
 //map global promise - get rid of warning
 
@@ -27,9 +30,30 @@ app.set('view engine', 'handlebars');
 
 //body parser middleware
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+
+//method override middleware
+app.use(methodOverride('_method'));
+//express-session
+app.use(session({
+    secret: 'bharathi',
+    resave: true,
+    saveUninitialized: true,
+ }));
+ //flash
+ app.use(flash());
+
+ //global variables for flash message
+
+ app.use(function(req,res,next){
+     res.locals.success = req.flash('success_msg');
+     res.locals.error_msg = req.flash('error_msg');
+     res.locals.error = req.flash('error');
+     next();
+ })
 
 
 //middleware
@@ -45,6 +69,8 @@ app.use(function (req, res, next) {
 // });
 
 
+
+
 app.get('/', function (req, res) {
     res.render('home');
 });
@@ -53,10 +79,25 @@ app.get('/about', (req,res)=>{
     res.render('about');
 });
 
+//add idea form
 app.get('/ideas/add',(req,res) =>{
     res.render('ideas/add');
 });
 
+//Edit idea form
+app.get('/ideas/edit/:id',(req,res) =>{
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+        res.render('ideas/edit',{
+           idea:idea 
+           
+        });
+    }); 
+});
+
+//process form
 app.post('/ideas',(req,res) => {
     // console.log(req.body);
     // res.send('ok');
@@ -84,10 +125,46 @@ app.post('/ideas',(req,res) => {
         new Idea(newUser)
           .save()
           .then(idea => {
+            req.flash('success_msg', 'idea added');
               res.redirect('/ideas');
           })
     }
 });
+
+app.get('/ideas',(req,res)=>{
+    Idea.find({})
+    .sort({date:'desc'})
+    .then(ideas =>{
+        res.render('ideas/index',{
+            ideas:ideas
+        });
+    })   
+});
+
+//Edit form process
+app.put('/ideas/:id',(req,res) => {
+Idea.findOne({
+    _id: req.params.id
+})
+.then(idea =>{
+    idea.title = req.body.title;
+    idea.details = req.body.details;
+    idea.save()
+    .then(idea =>{
+        req.flash('success_msg', 'idea updated');
+      res.redirect('/ideas');
+    })
+});
+});
+
+app.delete('/ideas/:id',(req,res)=>{
+   Idea.remove({_id:req.params.id})
+   .then(() => {
+       req.flash('success_msg', 'idea removed');
+       res.redirect('/ideas');
+   });
+});
+
 app.listen(port, () =>{
     console.log(`server started on port ${port}`);
 });
